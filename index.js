aaa81ef45da56ac2a165f345e65ba124f99e4636
Bittu <bittu1040@gmail.com>
2023-12-27 11:27:10 +0530

    Table

---
+++ index.js
@@ -0,0 +1,175 @@
+const data = [
+    {
+        "Sl": 1,
+        "First": "Johnnmmmm",
+        "Last": "Doe",
+        "City": "New York",
+        "Email": "john@example.com",
+        "Mobile": "1234567890",
+        "DOB": "1990-05-15"
+    },
+    {
+        "Sl": 2,
+        "First": "Alice",
+        "Last": "Smith",
+        "City": "Los Angeles",
+        "Email": "alice@example.com",
+        "Mobile": "9876543210",
+        "DOB": "1985-08-25"
+    },
+    {
+        "Sl": 3,
+        "First": "Michael",
+        "Last": "Johnson",
+        "City": "Chicago",
+        "Email": "michael@example.com",
+        "Mobile": "5555555555",
+        "DOB": "1992-03-10"
+    },
+    {
+        "Sl": 4,
+        "First": "Sarah",
+        "Last": "Williams",
+        "City": "Houston",
+        "Email": "sarah@example.com",
+        "Mobile": "3333333333",
+        "DOB": "1988-11-30"
+    },
+    {
+        "Sl": 5,
+        "First": "Robert",
+        "Last": "Brown",
+        "City": "San Francisco",
+        "Email": "robert@example.com",
+        "Mobile": "7777777777",
+        "DOB": "1975-09-12"
+    },
+    {
+        "Sl": 6,
+        "First": "Emily",
+        "Last": "Taylor",
+        "City": "Seattle",
+        "Email": "emily@example.com",
+        "Mobile": "8888888888",
+        "DOB": "1995-07-20"
+    },
+    {
+        "Sl": 7,
+        "First": "David",
+        "Last": "Anderson",
+        "City": "Miami",
+        "Email": "david@example.com",
+        "Mobile": "2222222222",
+        "DOB": "1980-04-05"
+    },
+    {
+        "Sl": 8,
+        "First": "Olivia",
+        "Last": "Miller",
+        "City": "Denver",
+        "Email": "olivia@example.com",
+        "Mobile": "9999999999",
+        "DOB": "1998-01-18"
+    },
+    {
+        "Sl": 9,
+        "First": "Daniel",
+        "Last": "Wilson",
+        "City": "Boston",
+        "Email": "daniel@example.com",
+        "Mobile": "4444444444",
+        "DOB": "1983-06-27"
+    },
+    {
+        "Sl": 10,
+        "First": "Sophia",
+        "Last": "Garcia",
+        "City": "Phoenix",
+        "Email": "sophia@example.com",
+        "Mobile": "6666666666",
+        "DOB": "1991-02-14"
+    },
+    {
+        "Sl": 11,
+        "First": "James",
+        "Last": "Martinez",
+        "City": "Philadelphia",
+        "Email": "james@example.com",
+        "Mobile": "1212121212",
+        "DOB": "1987-10-08"
+    },
+    {
+        "Sl": 12,
+        "First": "Isabella",
+        "Last": "Lopez",
+        "City": "Austin",
+        "Email": "isabella@example.com",
+        "Mobile": "7878787878",
+        "DOB": "1993-12-22"
+    },
+    {
+        "Sl": 13,
+        "First": "Logan",
+        "Last": "Hernandez",
+        "City": "Portland",
+        "Email": "logan@example.com",
+        "Mobile": "4545454545",
+        "DOB": "1989-08-17"
+    },
+    {
+        "Sl": 14,
+        "First": "Ava",
+        "Last": "Gonzalez",
+        "City": "Dallas",
+        "Email": "ava@example.com",
+        "Mobile": "6565656565",
+        "DOB": "1996-06-03"
+    },
+    {
+        "Sl": 15,
+        "First": "Mason",
+        "Last": "Perez",
+        "City": "Detroit",
+        "Email": "mason@example.com",
+        "Mobile": "9898989898",
+        "DOB": "1986-04-28"
+    }
+];
+
+function populateTable() {
+    const tableBody = document.getElementById('tableBody');
+
+    tableBody.innerHTML = '';
+
+    data.forEach(item => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>${item.Sl}</td>
+        <td>${item.First}</td>
+        <td>${item.Last}</td>
+        <td>${item.City}</td>
+        <td>${item.Email}</td>
+        <td>${item.Mobile}</td>
+        <td>${item.DOB}</td>  `;
+        tableBody.appendChild(row);
+    });
+}
+populateTable();
+
+function filterTable() {
+    const searchInput = document.getElementById('searchInput');
+    const searchTerm = searchInput.value.toLowerCase().trim();
+  
+    const filteredData = data.filter(item => {
+      // Customize the filtering logic as per your data structure
+      return Object.values(item).some(value =>
+        value.toString().toLowerCase().includes(searchTerm)
+      );
+    });
+  
+    populateTable(filteredData);
+  }
+  
+  // Event listener for search input
+  const searchInput = document.getElementById('searchInput');
+  searchInput.addEventListener('input', filterTable);
